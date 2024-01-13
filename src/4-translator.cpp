a997ca3472494087546aafd8432c87034fa167a4
eccentricOrange <eccentric.orange2@gmail.com>
2024-01-13 21:45:52 +0530

    working

--- src/4-translator.cpp
+++ src/4-translator.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <CAN.h>
-#include <settings.h>
+
+#include "settings.h"
 
 #define DEBUG true
 
@@ -8,90 +9,159 @@ char* readBytesFromSerial();
 void sendDataToOtherNodes(char* SerialData);
 
 void setup() {
-  Serial.begin(SERIAL_BAUD_RATE);
+    bool statusLEDState = LOW;
+    Serial.begin(SERIAL_BAUD_RATE);
+
+    pinMode(MAIN_STATUS_LED_PIN, OUTPUT);
+    pinMode(CAN_TX_LED_PIN, OUTPUT);
+    digitalWrite(CAN_TX_LED_PIN, LOW);
 
 #if DEBUG
-  Serial.println("Translator node");
+    Serial.println("Translator node");
 #endif
 
-  CAN.setPins(CRX_PIN, CTX_PIN);
+    CAN.setPins(CRX_PIN, CTX_PIN);
 
-  while (!CAN.begin(CAN_BAUD_RATE)) {
+    while (!CAN.begin(CAN_BAUD_RATE)) {
 #if DEBUG
-    Serial.println("Starting CAN failed!");
+        Serial.println("Starting CAN failed!");
 #endif
 
+        delay(100);
+        digitalWrite(MAIN_STATUS_LED_PIN, statusLEDState);
+        statusLEDState = !statusLEDState;
+    }
+
+#if DEBUG
+    Serial.println("can init success");
+#endif
+
+    digitalWrite(MAIN_STATUS_LED_PIN, HIGH);
     delay(100);
-  }
-
-#if DEBUG
-  Serial.println("can init success");
-#endif
+    digitalWrite(MAIN_STATUS_LED_PIN, LOW);
 }
 
 void loop() {
-  char SerialData[SERIAL_DATA_LENGTH];
+    char SerialData[SERIAL_DATA_LENGTH];
 
-  // justification to declare inside loop
-  // https://stackoverflow.com/a/7959658/10907280
+    // justification to declare inside loop
+    // https://stackoverflow.com/a/7959658/10907280
+    digitalWrite(CAN_TX_LED_PIN, HIGH);
 
-  while (Serial.available()) {
+    testNode();
+
+    while (Serial.available()) {
 #if DEBUG
-    Serial.println("sa i");
+        Serial.println("sa i");
 #endif
 
-    Serial.readBytes(SerialData, SERIAL_DATA_LENGTH);
-    sendDataToOtherNodes(SerialData);
-  }
+        Serial.readBytes(SerialData, SERIAL_DATA_LENGTH);
+
+        digitalWrite(CAN_TX_LED_PIN, HIGH);
+        sendDataToOtherNodes(SerialData);
+        digitalWrite(CAN_TX_LED_PIN, LOW);
+    }
+}
+
+void testNode() {
+    byte data[2];
+
+    for (size_t node = 0; node < TOTAL_NODE_COUNT; node++) {
+#if DEBUG
+        Serial.print("Sending to node: ");
+        Serial.println(FIRST_NODE_ADDRESS + node, HEX);
+#endif
+
+        for (int direction = 0; direction < 2; direction++) {
+            for (uint8_t speed = 0; speed < 255; speed++) {
+                digitalWrite(CAN_TX_LED_PIN, HIGH);
+                CAN.beginPacket(FIRST_NODE_ADDRESS + node);
+
+                data[0] = speed;
+                data[1] = direction;
+
+                CAN.write(data, BYTES_PER_NODE);
+
+                CAN.endPacket();
+
+                digitalWrite(CAN_TX_LED_PIN, LOW);
+
+                Serial.println(speed);
+
+                delay(25);
+            }
+
+            for (uint8_t speed = 255; speed > 0; speed--) {
+                digitalWrite(CAN_TX_LED_PIN, HIGH);
+                CAN.beginPacket(FIRST_NODE_ADDRESS + node);
+
+                data[0] = speed;
+                data[1] = direction;
+
+                CAN.write(data, BYTES_PER_NODE);
+
+                CAN.endPacket();
+
+                digitalWrite(CAN_TX_LED_PIN, LOW);
+
+                Serial.println(speed);
+
+                delay(25);
+            }
+        }
+
+#if DEBUG
+        Serial.print("Sent to node: ");
+        Serial.println(FIRST_NODE_ADDRESS + node, HEX);
+#endif
+    }
+
+#if DEBUG
+    Serial.println("Sent to all nodes");
+#endif
 }
 
 void sendDataToOtherNodes(char* SerialData) {
 #if DEBUG
-  Serial.print("read: ");
-  Serial.println(SerialData);
+    Serial.print("read: ");
+    Serial.println(SerialData);
 #endif
 
-  byte data[BYTES_PER_NODE];
+    byte data[BYTES_PER_NODE];
 
-  for (
-    size_t node = 0;
-    node < TOTAL_NODE_COUNT;
-    node++) {
-
+    for (size_t node = 0; node < TOTAL_NODE_COUNT; node++) {
 #if DEBUG
-    Serial.println("ctx i");
-    digitalWrite(CAN_TX_LED_PIN, HIGH);
+        Serial.println("ctx i");
+        digitalWrite(CAN_TX_LED_PIN, HIGH);
 #endif
 
-    CAN.beginPacket(FIRST_NODE_ADDRESS + node);
+        CAN.beginPacket(FIRST_NODE_ADDRESS + node);
 #if DEBUG
-    Serial.println("packet begun");
+        Serial.println("packet begun");
 #endif
 
-    data[0] = SerialData[node * 2];
-    data[1] = SerialData[node * 2 + 1];
+        data[0] = SerialData[node * 2];
+        data[1] = SerialData[node * 2 + 1];
 
-    CAN.write(data, BYTES_PER_NODE);
+        CAN.write(data, BYTES_PER_NODE);
 #if DEBUG
-    Serial.println("written");
+        Serial.println("written");
 #endif
 
-
-    CAN.endPacket();
+        CAN.endPacket();
 #if DEBUG
-    Serial.println("packet ended");
+        Serial.println("packet ended");
 #endif
 
-
-    digitalWrite(CAN_TX_LED_PIN, LOW);
+        digitalWrite(CAN_TX_LED_PIN, LOW);
 
 #if DEBUG
-    Serial.print("Sent to node: ");
-    Serial.println(FIRST_NODE_ADDRESS + node, HEX);
+        Serial.print("Sent to node: ");
+        Serial.println(FIRST_NODE_ADDRESS + node, HEX);
 #endif
-  }
+    }
 
 #if DEBUG
-  Serial.println("Sent to all nodes");
+    Serial.println("Sent to all nodes");
 #endif
 }
--- src/5-generic-node.cpp
+++ src/5-generic-node.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <CAN.h>
-#include <settings.h>
+#include "settings.h"
 
 #define DEBUG true
 
@@ -8,112 +8,125 @@ void ReadCANWriteMotors(int packetSize);
 byte readDipSwitch();
 
 void setup() {
+  bool statusLEDState = LOW;
 
-    #if DEBUG
-        Serial.begin(SERIAL_BAUD_RATE);
+#if DEBUG
+  Serial.begin(SERIAL_BAUD_RATE);
 
-        while (!Serial) {
-            delay(100);
-        }
+  while (!Serial) {
+    delay(100);
+  }
 
-        Serial.println("Arm node 1");
-    #endif
+  Serial.println("Arm node 1");
+#endif
 
-    pinMode(ADDRESS_DIP_1_PIN, INPUT);
-    pinMode(ADDRESS_DIP_2_PIN, INPUT);
-    pinMode(ADDRESS_DIP_3_PIN, INPUT);
-    pinMode(ADDRESS_DIP_4_PIN, INPUT);
+  pinMode(ADDRESS_DIP_1_PIN, INPUT);
+  pinMode(ADDRESS_DIP_2_PIN, INPUT);
+  pinMode(ADDRESS_DIP_3_PIN, INPUT); 
+  pinMode(ADDRESS_DIP_4_PIN, INPUT);
 
-    pinMode(PWM_PIN, OUTPUT);
-    pinMode(DIR_PIN, OUTPUT);
+  pinMode(PWM_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
 
-    pinMode(MAIN_STATUS_LED_PIN, OUTPUT);
-    pinMode(CAN_SYNC_LED_PIN, OUTPUT);
-    pinMode(CAN_RX_LED_PIN, OUTPUT);
-    pinMode(CAN_TX_LED_PIN, OUTPUT);
-    pinMode(IMU_CALIBRATION_LED_PIN, OUTPUT);
+  pinMode(MAIN_STATUS_LED_PIN, OUTPUT);
+  pinMode(CAN_SYNC_LED_PIN, OUTPUT);
+  pinMode(CAN_RX_LED_PIN, OUTPUT);
+  pinMode(CAN_TX_LED_PIN, OUTPUT);
+  pinMode(IMU_CALIBRATION_LED_PIN, OUTPUT);
 
-    digitalWrite(CAN_TX_LED_PIN, LOW);  // not implemented
-    digitalWrite(IMU_CALIBRATION_LED_PIN, LOW);  // not implemented
+  digitalWrite(CAN_TX_LED_PIN, LOW);           // initial
+  digitalWrite(CAN_RX_LED_PIN, LOW);           // initial
+  digitalWrite(IMU_CALIBRATION_LED_PIN, LOW);  // not implemented
 
-    CAN.setPins(CRX_PIN, CTX_PIN);
 
-    while (!CAN.begin(CAN_BAUD_RATE)) {
+  CAN.setPins(CRX_PIN, CTX_PIN);
 
-        #if DEBUG
-            Serial.println("Starting CAN failed!");
-        #endif
+  while (!CAN.begin(CAN_BAUD_RATE)) {
 
-        delay(100);
-    }
+#if DEBUG
+    Serial.println("Starting CAN failed!");
+#endif
 
-    #if DEBUG
-        Serial.println("CAN Initialized successfully.");
-    #endif
+    delay(100);
 
-    #if DEBUG
-        Serial.println(readDipSwitch());
-    #endif
+    delay(100);
+    digitalWrite(MAIN_STATUS_LED_PIN, statusLEDState);
+    statusLEDState = !statusLEDState;
+  }
+
+#if DEBUG
+  Serial.println("CAN Initialized successfully.");
+#endif
+
+  digitalWrite(MAIN_STATUS_LED_PIN, HIGH);
+  delay(100);
+  digitalWrite(MAIN_STATUS_LED_PIN, LOW);
+
+#if DEBUG
+  Serial.println(readDipSwitch());
+#endif
 }
 
 void loop() {
-    int packetSize = CAN.parsePacket();
-    
-    ReadCANWriteMotors(packetSize);
+  int packetSize = CAN.parsePacket();
+
+  ReadCANWriteMotors(packetSize);
 }
 
 void ReadCANWriteMotors(int packetSize) {
-    /**
+  /**
      * @brief Reads CAN packet and writes to motor controller
      * @param packetSize Size of CAN packet
      * @details direction: 0 ==> stop, 1 ==> forward, -1 ==> backward
      * @details motorSpeed: 0-255
     */
 
-    int motorSpeed = 0;
-    int direction = 0;
-    static bool CAN_RX_LED_State = false;
-    char data[BYTES_PER_NODE];
+  int motorSpeed = 0;
+  int direction = 0;
+  static bool CAN_RX_LED_State = false;
+  char data[BYTES_PER_NODE];
 
-    if (packetSize && (CAN.packetId() == readDipSwitch())) {
+  if (packetSize && (CAN.packetId() == readDipSwitch())) {
+    digitalWrite(CAN_RX_LED_PIN, HIGH);
 
-        #if DEBUG
-            Serial.println("Received packet for this node.");
-        #endif
+#if DEBUG
+    Serial.println("Received packet for this node.");
+#endif
 
-        while (CAN.available()) {
-            digitalWrite(CAN_RX_LED_PIN, HIGH);
-            CAN.readBytes(data, BYTES_PER_NODE);
-            motorSpeed = data[0];
-            direction = data[1];
-        }
-
-        #if DEBUG
-            Serial.print("Motor speed: ");
-            Serial.println(motorSpeed);
-        #endif
-
-        digitalWrite(DIR_PIN, direction > 0);
-        analogWrite(PWM_PIN, motorSpeed * (direction != 0));
-        digitalWrite(CAN_SYNC_LED_PIN, LOW);
+    while (CAN.available()) {
+      CAN.readBytes(data, BYTES_PER_NODE);
+      motorSpeed = data[0];
+      direction = data[1];
     }
+
+#if DEBUG
+    Serial.print("Motor speed: ");
+    Serial.print(motorSpeed);
+    Serial.print(", direction: ");
+    Serial.println(direction);
+#endif
+
+    digitalWrite(DIR_PIN, direction);
+    analogWrite(PWM_PIN, motorSpeed);
+    digitalWrite(CAN_RX_LED_PIN, LOW);
+  }
 }
 
 byte readDipSwitch() {
-    byte address = 0;
+  byte address = 0;
 
-    address = digitalRead(ADDRESS_DIP_1_PIN);
-    address <<= 1;
-    address += digitalRead(ADDRESS_DIP_2_PIN);
-    address <<= 1;
-    address += digitalRead(ADDRESS_DIP_3_PIN);
-    address <<= 1;
-    address += digitalRead(ADDRESS_DIP_4_PIN);
+  address = digitalRead(ADDRESS_DIP_1_PIN);
+  address <<= 1;
+  address += digitalRead(ADDRESS_DIP_2_PIN);
+  address <<= 1;
+  address += digitalRead(ADDRESS_DIP_3_PIN);
+  address <<= 1;
+  address += digitalRead(ADDRESS_DIP_4_PIN);
 
-    #if DEBUG
-        Serial.print("Address: ");
-        Serial.println(address, HEX);
-    #endif
+#if DEBUG
+  Serial.print("Address: ");
+  Serial.println(address, HEX);
+#endif
 
-    return address;
+  return address;
 }
